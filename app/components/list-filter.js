7b03bd67811287c996bd5832964232c5660c7927
Gregor <gregor@martynus.net>
2016-06-10 09:12:33 -0400

    Step: "Building a Complex Compoment" broke it
    https://guides.emberjs.com/v2.6.0/tutorial/autocomplete-component/

---
+++ app/components/list-filter.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['list-filter'],
+  value: '',
+
+  init() {
+    this._super(...arguments);
+    this.get('filter')('').then((results) => {
+      this.set('rentals', results)
+    });
+  },
+
+  actions: {
+    handleFilterEntry() {
+      let filterInputValue = this.get('value');
+      let filterAction = this.get('filter');
+      filterAction(filterInputValue).then((filterResults) => this.set('rentals', filterResults));
+    }
+  }
+});
--- app/helpers/rental-property-type.js
+++ app/helpers/rental-property-type.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
-export function rentalPropertyType(params/*, hash*/) {
-  return params;
+const communityPropertyTypes = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
+
+export function rentalPropertyType([type]/*, hash*/) {
+  if (communityPropertyTypes.contains(type)) {
+    return 'Community';
+  }
+
+  return 'Standalone';
 }
 
 export default Ember.Helper.helper(rentalPropertyType);
